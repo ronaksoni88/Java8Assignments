7eed1eee3f029adc1bdb08bf24861c4676b9b2b0
ronak soni <soni.ronak88@gmail.com>
2016-03-09 00:32:44 +0530

    third push

--- src/java8/AssignmentTwo.java
+++ src/java8/AssignmentTwo.java
@@ -1,8 +1,10 @@
 package java8;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class AssignmentTwo
 {
@@ -10,6 +12,15 @@ public class AssignmentTwo
 	{
 		for(T i : list)
 			consumer.accept(i);
+	}
+	
+	public static <T,R> List<R> StringToLength(List<T> list, Function<T, R> func)
+	{
+		List<R> returnList = new ArrayList<>();
+		for(T i : list)
+			returnList.add(func.apply(i));
+		
+		return returnList;
 	}
 	
 	public static void main(String[] args)
@@ -24,5 +35,10 @@ public class AssignmentTwo
 		toConvert(listOfString,l1 -> System.out.println("Length of "+l1 +" is : " +l1.length()));
 		
 		toConvert(listOfString, l1 -> System.out.println("Block it : " +l1.toUpperCase()));
+		
+		List<Integer> list = StringToLength(listOfString , l1 -> l1.length());
+		System.out.println(list);
 	}
+	
+	
 }
